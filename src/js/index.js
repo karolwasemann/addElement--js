2c14c025e45baf57c81af57e736f1e20364c827f
karolwasemann <kw.kwaq@gmail.com>
2020-11-12 13:42:52 +0100

    feat: js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,23 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+const btn = document.querySelector(".add");
+let number = 1;
+
+const addElement = function () {
+  console.log("klik");
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  div.classList.add("square");
+  console.log(number);
+
+  if (number % 5 == 0) {
+    div.classList.add("circle");
+  }
+  div.innerHTML = number;
+  number++;
+};
+
+btn.addEventListener("click", addElement);
